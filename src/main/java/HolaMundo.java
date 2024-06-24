4a668d580ff19fd57f6265bb9bd93f956b8e12a7
Teo <mateomoralesgil@hotmail.com>
2024-06-24 09:49:17 -0500

    Prueba git

--- src/main/java/HolaMundo.java
+++ src/main/java/HolaMundo.java
@@ -1,7 +1,7 @@
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
-        System.out.println("Adios");
+        System.out.println("Hola Meli");
+        System.out.println("Adios meli");
     }
 }
 
